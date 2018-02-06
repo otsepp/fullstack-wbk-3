b55605ceef388c40904b1068d31c1d136043d452
otsepp <tuomas.seppanen@helsinki.fi>
2018-02-06 09:55:21 +0200

    mongo osoite korjattu

--- models/person.js
+++ models/person.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 
- require('dotenv').config()
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
 const url = process.env.MONGODB_URL
 mongoose.connect(url)
 
